a2d0f48e9c5e608d983c9fdf7aa84b92ecc05c18
baptiste.chocot@gmail.com <baptiste.chocot@gmail.com>
2019-03-01 11:53:16 +0100

    refactor code; see TODO to continue + SensorsActivity

--- app/src/main/java/com/example/chocot1u/jpro/service/CheckService.java
+++ app/src/main/java/com/example/chocot1u/jpro/service/CheckService.java
@@ -1,20 +1,13 @@
 package com.example.chocot1u.jpro.service;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
-
-import com.example.chocot1u.jpro.R;
-import com.example.chocot1u.jpro.SensorsActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,18 +17,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CheckService extends Service {
-    private final String API_URL = "http://iotlab.telecomnancy.eu/rest/data/1/light1-light2/last";
+    private final String MOTES_URL = "http://iotlab.telecomnancy.eu/rest/info/motes";
+    private final String DATA_URL = "http://iotlab.telecomnancy.eu/rest/data/1/light1-light2/last";
 
     private final static AtomicInteger c = new AtomicInteger(0);
     public static int getID() {
@@ -65,7 +62,7 @@ public class CheckService extends Service {
     // service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("CheckService","Setting mode to sticky");
+        // Log.d("CheckService","Setting mode to sticky");
         return START_STICKY;
     }
 
@@ -87,67 +84,53 @@ public class CheckService extends Service {
             @Override
             public void run() {
                 try {
-                    Log.d("CheckService", String.format("[TimerTask] GET %s", API_URL));
-                    HttpURLConnection conn = (HttpURLConnection) new URL(API_URL).openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setConnectTimeout(1000);
-                    conn.setReadTimeout(1000);
-                    conn.connect();
-                    Reader reader = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                    BufferedReader rd = new BufferedReader(reader);
-                    StringBuilder content = new StringBuilder();
-                    String line;
-                    while ((line = rd.readLine()) != null) {
-                        content.append(line).append("\n");
+                    // discover motes
+                    JSONObject response = new JSONObject(askURL(MOTES_URL));
+                    JSONArray motes = response.getJSONArray("sender");
+                    int motesNb = response.getInt("motesNb");
+                    // get data and read it
+                    JSONArray data = new JSONObject(askURL(DATA_URL)).getJSONArray("data");
+                    ArrayList<Integer> lit_lights = new ArrayList<>();
+                    for (int i=0; i<data.length(); i++) {
+                        JSONObject light_data = data.getJSONObject(i);
+                        if (light_data.getDouble("value") > 500.0) {
+                            lit_lights.add(i);
+                        }
                     }
-                    conn.disconnect();
-                    // analysing response
-                    JSONObject json = new JSONObject(content.toString());
-                    JSONArray data = json.getJSONArray("data");
-                    // sensor 1
-                    JSONObject sensor1Data = data.getJSONObject(0);
-                    long timestamp1 = sensor1Data.getLong("timestamp");
-                    double light1Value = sensor1Data.getDouble("value");
-                    String mote1 = sensor1Data.getString("mote");
-                    boolean isLight1On = light1Value > 275;
-                    // sensor 2
-                    JSONObject sensor2Data = data.getJSONObject(3);
-                    long timestamp2 = sensor2Data.getLong("timestamp");
-                    double light2Value = sensor2Data.getDouble("value");
-                    String mote2 = sensor2Data.getString("mote");
-                    boolean isLight2On = light2Value > 275;
-                    // let's go
-                    if (isLight1On || isLight2On) {
-                        Log.d("CheckService", "A light is on.");
-                        // checking if we should notify or not
-                        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(instance);
-                        DateFormat formater = new SimpleDateFormat("hh:mm");
-                        // notification
-                        long w_start_hour = formater.parse(sp.getString("w_start_hour", "19:00")).getTime();
-                        long w_end_hour = formater.parse(sp.getString("w_end_hour", "23:00")).getTime();
-                        // mail
-                        long we_start_hour = formater.parse(sp.getString("we_start_hour", "23:00")).getTime();
-                        long we_end_hour = formater.parse(sp.getString("we_end_hour", "06:00")).getTime();
-                        // current time
-                        long current_time = (System.currentTimeMillis() / 1000) % 86400000;
-                        boolean w = current_time <= w_end_hour && w_start_hour <= current_time;
-                        boolean we = current_time < we_end_hour && we_start_hour <= current_time;
-                        Log.d("DEBUG", ""+w_start_hour+" "+current_time+" "+w_end_hour);
-                        if (!w && !we) {
-                            return;
+                    if (lit_lights.size() != 0) {
+                        for (int i : lit_lights) {
+                            JSONObject light_data = data.getJSONObject(i);
+                            // first check if we need to notify or not
+                            long timestamp = light_data.getLong("timestamp") / 1000;
+                            timestamp %= 84600;
+                            boolean isWeekNotificationTime = isWeekNotificationTime(timestamp);
+                            boolean isWeekEndNotificationTime = isWeekEndNotificationTime(timestamp);
+                            if (!isWeekNotificationTime && !isWeekEndNotificationTime) {
+                                return;
+                            }
+                            // if we're here, we have to notify
+                            JSONObject mote = null;
+                            for (int j=0; j<motes.length(); j++) {
+                                JSONObject tmpMote = motes.getJSONObject(j);
+                                if (tmpMote.getString("mac").equals(light_data.getString("mote"))) {
+                                    mote = tmpMote;
+                                    break;
+                                }
+                            }
+                            assert mote != null; // might never happen
+                            // now send notification of mal
+                            if (isWeekNotificationTime) {
+                                // notification
+                                // TODO
+                            }
+                            if (isWeekEndNotificationTime) {
+                                // mail
+                                // TODO
+                            }
                         }
-                        // notifying
-                        String msg = "";
-                        if (isLight1On) {
-                            SimpleDateFormat formatter = new SimpleDateFormat("[hh:mm:ss]");
-                            String ts = formatter.format(new Date(timestamp1));
-                            msg += String.format("[%s] Light 1 was turned on at %s.\n", mote1, ts);
-                        }
-                        if (isLight2On) {
-                            SimpleDateFormat formatter = new SimpleDateFormat("[hh:mm:ss]");
-                            String ts = formatter.format(new Date(timestamp2));
-                            msg += String.format("[%s] Light 2 was turned on at %s.\n", mote2, ts);
-                        }
+                    }
+
+                    /*
                         if (w) {
                             // preparing intent
                             Intent notificationIntent = new Intent(instance, SensorsActivity.class);
@@ -177,15 +160,70 @@ public class CheckService extends Service {
                             startActivity(Intent.createChooser(mailIntent, "Alert supervisor"));
                             Log.d("CheckService", "Sent mail.");
                         }
-                    }
-                } catch (IOException e) {
-                    Log.d("CheckService", "URL is malformed. please contact devs.");
+                    }*/
                 } catch (JSONException e) {
                     Log.d("CheckService", "Response is not JSON formatted.");
-                } catch (ParseException e) {
-                    Log.d("CheckService", "Unable to parse hour.");
                 }
             }
         };
+    }
+
+    private String askURL(String url) {
+        try {
+            Log.d("CheckService", String.format("[TimerTask] GET %s", url));
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(1000);
+            conn.setReadTimeout(1000);
+            conn.connect();
+            Reader reader = new InputStreamReader(conn.getInputStream(), "UTF-8");
+            BufferedReader rd = new BufferedReader(reader);
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            conn.disconnect();
+            return content.toString();
+        } catch (UnsupportedEncodingException e) {
+            Log.d("CheckService", "UnsupportedEncodingException");
+        } catch (ProtocolException e) {
+            Log.d("CheckService", "ProtocolException");
+        } catch (MalformedURLException e) {
+            Log.d("CheckService", "MalformedURLException");
+        } catch (IOException e) {
+            Log.d("CheckService", "IOException");
+        }
+        return "";
+    }
+
+    private boolean isWeekNotificationTime(long timestamp) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        DateFormat formatter = new SimpleDateFormat("hh:mm");
+        try {
+            long w_start_hour = formatter.parse(sp.getString("w_start_hour", "19:00")).getTime();
+            long w_end_hour = formatter.parse(sp.getString("w_end_hour", "23:00")).getTime();
+            if (w_end_hour < w_start_hour)
+                w_end_hour += 84600;
+            return timestamp >= w_start_hour && timestamp <= w_end_hour;
+        } catch (ParseException e) {
+            Log.d("CheckService", "ParseException");
+        }
+        return false;
+    }
+
+    private boolean isWeekEndNotificationTime(long timestamp) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm");
+        try {
+            long we_start_hour = formatter.parse(sp.getString("we_start_hour", "23:00")).getTime();
+            long we_end_hour = formatter.parse(sp.getString("we_end_hour", "06:00")).getTime();
+            if (we_end_hour < we_start_hour)
+                we_end_hour += 84600;
+            return timestamp >= we_start_hour && timestamp <= we_end_hour;
+        } catch (ParseException e) {
+            Log.d("CheckService", "ParseException");
+        }
+        return false;
     }
 }
